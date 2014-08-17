75c77aa3c4a677c35b6dc651aaeac1ff46d193e1
martinsveda <martin.sveda@email.cz>
2014-08-17 20:42:22 +0200

    Pridani AuthenticateUser

---
+++ Lunchtime/AuthenticationService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunchtime
+{
+    public interface IAuthenticationService
+    {
+        User AuthenticateUser(string username; string password);
+    }
+
+    public class AuthenticationService : IAuthenticationService
+    {
+        private class InternalUserData
+        {
+            public InternalUserData(string username, string email, string hashedPassword, string[] roles)
+            {
+                Username = username;
+                Email = email;
+                HashedPassword = hashedPassword;
+                Roles = roles;
+            }
+
+            public string Username
+            {
+                get ;
+                private set;
+            }
+
+            public string Email
+            {
+                get;
+                private set;
+            }
+
+            public string HashedPassword
+            {
+                get;
+                private set;
+            }
+
+            public string[] Roles
+            {
+                get ;
+                private set;
+            }
+
+        }
+    }
+}
