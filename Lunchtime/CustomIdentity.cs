cafd5ca7acc481d9dbb78f50b43e7d59cc2c67d9
martinsveda <martin.sveda@email.cz>
2014-08-17 20:25:39 +0200

    Pridani trid IComman, IIdentity a IPrincipal

---
+++ Lunchtime/CustomIdentity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Principal;
+
+namespace Lunchtime
+{
+    class CustomIdentity : IIdentity
+    {
+        public CustomIdentity(string name, string email, string[] roles)
+        {
+            Name = name;
+            Email = email;
+            Roles = roles;
+        }
+
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string[] Roles { get; private set; }
+
+        #region IIdentity members
+        public string AuthenticationType { get { return "Custom authentication"; } }
+        public bool IsAuthenticated { get { return !string.IsNullOrEmpty(Name); } }
+        #endregion
+    }
+
+
+    class AnonymousIdentity : CustomIdentity
+    {
+        public AnonymousIdentity() : base(string.Empty, string.Empty, new string[] { })
+        {
+            ;
+        }
+    }
+}
