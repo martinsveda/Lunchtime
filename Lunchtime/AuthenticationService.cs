45f15c8fcb2e5f6e4abdfe69ca68f66237cee84c
martinsveda <martin.sveda@email.cz>
2014-08-17 22:13:49 +0200

    Upgrade AuthenticationService

--- Lunchtime/AuthenticationService.cs
+++ Lunchtime/AuthenticationService.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+
 
 namespace Lunchtime
 {
     public interface IAuthenticationService
     {
-        User AuthenticateUser(string username; string password);
+        User AuthenticateUser(string username, string password);
     }
+
+
+    public class User
+    {
+        public User(string username, string email, string[] roles)
+        {
+            Username = username;
+            Email = email;
+            Roles = roles;
+        }
+
+        public string Username
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string[] Roles
+        {
+            get;
+            set;
+        }
+    }
+
 
     public class AuthenticationService : IAuthenticationService
     {
@@ -45,6 +77,20 @@ namespace Lunchtime
             {
                 get ;
                 private set;
+            }
+
+            public User AuthenticateUser(string username, string password)
+            {
+                return new User(string.Empty , string.Empty, null);
+            }
+
+            private string CalculateHash(string clearTextPassword, string salt)
+            {
+                byte[] saltedHashBytes = Encoding.UTF8.GetBytes(clearTextPassword + salt);
+                HashAlgorithm algorithm = new SHA256Managed();
+                byte[] hash = algorithm.ComputeHash(saltedHashBytes);
+
+                return Convert.ToBase64String(hash);
             }
 
         }
--- Lunchtime/User.cs
+++ Lunchtime/User.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿/*
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
-
 
 
 namespace Lunchtime
@@ -77,3 +77,5 @@ namespace Lunchtime
 
     }
 }
+
+*/
