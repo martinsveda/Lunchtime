f03c926d738e38eae1c9d04e3d032af1f640d80c
martinsveda <martin.sveda@email.cz>
2014-08-28 16:06:19 +0200

    test keyqords

--- Lunchtime/App.xaml.cs
+++ Lunchtime/App.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿/* Git revision information:
+ * 
+ * @version $Revision: 1e7f611039399b32e9000ec454609a0641dde368 $
+ * @author  $Author: Eugen Mihailescu <eugenmihailescux@gmail.com> $
+ * @date    $Date:   Thu May 3 01:17:45 2012 +0200 $
+ * 
+ * $Id:  | Thu May 3 01:17:45 2012 +0200 | Eugen Mihailescu  $
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
